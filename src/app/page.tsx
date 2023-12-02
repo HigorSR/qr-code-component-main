0d3b8fba3b8f923101daca08a24d4dc696443db3
Higor Rocha <higor_36rocha@hotmail.com>
2023-12-01 22:13:59 -0300

    Created card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Card } from "@/components/card";
+
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-Light-gray">
+      <Card/>
     </div>
   )
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import qr_code from "../../public/image-qr-code.png"
+
+export function Card() {
+    return (
+        <div className="bg-white rounded-3xl p-4 w-[330px]">
+            <Image src={qr_code} alt="qr-code image" className="rounded-2xl mb-6" />
+            <h1 className="text-Dark-blue text-2xl font-bold text-center mb-4">Improve your front-end skills by building projects</h1>
+            <p className="text-Grayish-blue text-center px-5 mb-6 leading-5">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+    )
+}
