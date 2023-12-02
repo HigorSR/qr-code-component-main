c0c56315b5da7f14d8f6645db621e4f993991418
Higor Rocha <higor_36rocha@hotmail.com>
2023-12-01 22:14:10 -0300

    Created Card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,7 +5,7 @@ export function Card() {
     return (
         <div className="bg-white rounded-3xl p-4 w-[330px]">
             <Image src={qr_code} alt="qr-code image" className="rounded-2xl mb-6" />
-            <h1 className="text-Dark-blue text-2xl font-bold text-center mb-4">Improve your front-end skills by building projects</h1>
+            <h1 className="text-Dark-blue text-2xl font-bold text-center mb-4 font-">Improve your front-end skills by building projects</h1>
             <p className="text-Grayish-blue text-center px-5 mb-6 leading-5">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
     )
